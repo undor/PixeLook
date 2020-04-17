0f752a5bd58beb4a57aba93a0d9207bd7a8149e3
Dor Arad <45083160+araddor1@users.noreply.github.com>
2020-04-17 17:28:19 +0300

    Add files via upload

---
+++ VideoCapture.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tkinter as tk
+import cv2
+
+
+def open_webcam():
+    cap = cv2.VideoCapture(1)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    while (True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,8 @@
+from VideoCapture import *
+
+def main():
+    print("hello world!")
+    open_webcam()
+
+if __name__ == "__main__":
+    main()
