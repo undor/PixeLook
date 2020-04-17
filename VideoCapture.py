aedefae2beaa82356fcdd94c8cbb5be432a6d2f9
Dor Arad <45083160+araddor1@users.noreply.github.com>
2020-04-17 17:39:53 +0300

    Update VideoCapture.py

--- VideoCapture.py
+++ VideoCapture.py
@@ -8,12 +8,7 @@ def open_webcam():
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
     while (True):
-        # Capture frame-by-frame
         ret, frame = cap.read()
-
-        # Our operations on the frame come here
-
-        # Display the resulting frame
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
