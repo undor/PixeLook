5e92ab669cf8ca3afa3a5e7ede6ca2afe36b9606
Dor Arad <45083160+araddor1@users.noreply.github.com>
2020-10-04 16:47:38 +0300

    full_face_csv is up

--- FullFaceSolution/model/FullFaceModel.py
+++ FullFaceSolution/model/FullFaceModel.py
@@ -1,10 +1,8 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
-
-import numpy as np 
-
 from PIL import Image
 from torchvision import transforms
 
