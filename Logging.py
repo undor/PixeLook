d34bd83793bb282c388be1bfe0382092732e80eb
Tomer Keren <tomerkeren42@gmail.com>
2021-04-06 09:54:04 +0300

    Added mkdir for outputs

--- Logging.py
+++ Logging.py
@@ -1,12 +1,16 @@
 from datetime import datetime
+import os.path
 
 
 def create_time_file_name(init, file_type):
-    return "outputs/" +init + "_" + datetime.now().strftime("%d_%m_%H_%M") + "." +file_type
+    if not os.path.exists("outputs/"):
+        os.mkdir("outputs/")
+    return "outputs/" + init + "_" + datetime.now().strftime("%d_%m_%H_%M") + "." + file_type
+
 
 class Logging:
     def __init__(self):
-        self.file = open(create_time_file_name("PixeLookLogging","csv"), "a")
+        self.file = open(create_time_file_name("PixeLookLogging", "csv"), "a")
         self.file.write("time,x,y\n")
 
     def add_pixel(self, pixel,cur_time):
@@ -17,6 +21,7 @@ class Logging:
     def close(self):
         self.file.close()
 
+
 class Logging_test():
     def __init__(self):
         self.file = open(create_time_file_name("PixelookTestLog","csv"), "a")
@@ -25,7 +30,6 @@ class Logging_test():
     def add_pixel(self, tag_pixel,cur_pixel,cur_time,errors):
         time = datetime.now() if cur_time is None else cur_time
         cur_time_str = time.strftime("%H:%M:%S:%f")
-        print(tag_pixel,cur_pixel)
         self.file.write(cur_time_str + "," + str(int(tag_pixel[0])) + "," + str(int(tag_pixel[1])))
         self.file.write("," + str(int(cur_pixel[0])) + "," + str(int(cur_pixel[1])))
         for e in errors:
